606981018ee010adfda943d0447fdaede050b136
Priyanshu1982 <23f2004528@ds.study.iitm.ac.in>
2024-11-05 13:30:30 +0530

    day3

--- app.py
+++ app.py
@@ -2,18 +2,19 @@
 from flask import Flask
 from backend.models import db
 
-app=None #app instance is not created.
+app=None #app instance is not created. #global variable(just a declaration)
 
 def setup_app():
-    app=Flask(__name__)
+    app=Flask(__name__) #Flask object is created
     
     app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///ticket_show.sqlite3" #having database file
     db.init_app(app) #connection between app and database
-    app.debug=True
     app.app_context().push() #direct access to other modules 
+    app.debug=True
     print("Ticket show app is started....")
 
 # call the setup
+#we call also do it like app=setup_app() instead defining app=None
 setup_app()
 
 #for many controllers we are using below
@@ -23,5 +24,5 @@ def home():
     return render_template("index.html")
 
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(debug=True) #instance is created
 
--- backend/controllers.py
+++ backend/controllers.py
@@ -17,9 +17,26 @@ def signin():
         usr=User_Info.query.filter_by(email=username,password=password).first() #remember mistake it's not a method query is not a method so remove braces after query
         if usr and usr.role==0:   #it means it exists and is admin
             return render_template("admin_dashboard.html")
+        elif usr and usr.role==1:
+            return render_template("user_dashboard.html")
+        else:
+            return render_template("login.html",msg="Invalid user credentials")
         #validation code ends
     return render_template("login.html")
 #for register
-@app.route("/register")
+@app.route("/register",methods=["GET","POST"])
 def signup():
+    if request.method=="POST":
+        uname=request.form.get("Email")
+        pwd=request.form.get("Password")
+        fullname=request.form.get("Fullname")
+        location=request.form.get("Location")
+        postal=request.form.get("Zip")
+        usr=User_Info.query.filter_by(email=uname).first()
+        if usr:
+            return render_template("signup.html",msg="Already existed user")
+        new_usr=User_Info(email=uname,password=pwd,full_name=fullname,address=location,pincode=postal)
+        db.session.add(new_usr)
+        db.session.commit()
+        return render_template("login.html",msg="Successfully registered")
     return render_template("signup.html")
