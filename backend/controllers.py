3f858728c6b3cbdb4c4ba6b88a15cf78bb440ee9
Priyanshu1982 <23f2004528@ds.study.iitm.ac.in>
2024-11-06 10:34:27 +0530

    venues

--- backend/controllers.py
+++ backend/controllers.py
@@ -1,5 +1,5 @@
 #App routes
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,url_for,redirect
 from .models import *
 from flask import current_app as app
 
@@ -7,6 +7,14 @@ from flask import current_app as app
 def home():
     return render_template("index.html")
 
+@app.route("/admin/<name>")
+def admin(name):
+    theatres=get_theatres()
+    return render_template("admin_dashboard.html",name=name,theatres=theatres)
+
+@app.route("/user/<name>")
+def user(name):
+    return render_template("user_dashboard.html",name=name)
 #for login
 @app.route("/login" ,methods=["GET","POST"])#requests by default is GET
 def signin():
@@ -16,9 +24,9 @@ def signin():
         password=request.form.get("Password")
         usr=User_Info.query.filter_by(email=username,password=password).first() #remember mistake it's not a method query is not a method so remove braces after query
         if usr and usr.role==0:   #it means it exists and is admin
-            return render_template("admin_dashboard.html")
+            return redirect(url_for("admin",name=username))
         elif usr and usr.role==1:
-            return render_template("user_dashboard.html")
+            return redirect(url_for("user",name=username))
         else:
             return render_template("login.html",msg="Invalid user credentials")
         #validation code ends
@@ -40,3 +48,21 @@ def signup():
         db.session.commit()
         return render_template("login.html",msg="Successfully registered")
     return render_template("signup.html")
+#for venue making
+@app.route("/venue/<name>",methods=["GET","POST"])#<> is parameter (from frontend to backend)
+def addvenue(name): #() for parameter
+    if request.method=="POST":
+        vname=request.form.get("name")
+        loc=request.form.get("location")
+        pin=request.form.get("pincode")
+        capacity=request.form.get("capacity")
+        new_theatre=Theatre(name=vname,location=loc,pincode=pin,capacity=capacity)
+        db.session.add(new_theatre)
+        db.session.commit()
+        return redirect(url_for("admin",name=name))
+    return render_template("addvenue.html",name=name)
+
+def get_theatres():
+    theatres=Theatre.query.all()
+    return theatres
+
